1f00706d8aa16eab721429b3f0d5129dd9de8096
JackeGould <jmgould913@gmail.com>
2024-01-16 20:03:24 -0500

    Code added to JavaScript file, comments added, README file added

--- script.js
+++ script.js
@@ -4,20 +4,60 @@ var generateBtn = document.querySelector("#generate");
 var generatePassword = function () {
 
   // Prompts user after clicking generate password
-  var passwordLength = window.prompt("How many characters would you like your password to contain?");
+  var passwordLength = window.prompt("How many characters would you like your password to contain?")
 
-  // If user enters value less than 8 or more than 128, window prompts new altert
+  // If user enters value less than 8 or more than 128, window prompts new alert
   if (passwordLength < 8 || passwordLength > 128) {
-  generatePassword()
-  } else { 
-  } }
+    generatePassword()
+  } else {
+    // Window prommpt OK/Cancel if user would like to apply specific characters
+    var types = window.confirm("Click OK to confirm inlcuding special characters")
+    var numbers = window.confirm("Click OK to confirm inlcuding numeric characters")
+    var lowercase = window.confirm("Click OK to confirm inlcuding lowercase characters")
+    var uppercase = window.confirm("Click OK to confirm inlcuding uppercase characters")
+
+    var specialCharacters = "!@#$%^&*-?"
+    var numericCharacters = 1234567890
+    var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+    var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    // Empty string to be later defined
+    var allCharacters = "";
+    var finalPassword = "";
+
+    // If types is true include specialCharacters
+    if (types) {
+      allCharacters += specialCharacters
+    }
+
+    // If numbers is true include numericCharacters
+    if (numbers) {
+      allCharacters += numericCharacters
+    }
+
+    // If lowercase is true include lowercaseCharacters
+    if (lowercase) {
+      allCharacters += lowercaseCharacters
+    }
+
+    // If uppercase is true include uppercaseCharacters
+    if (uppercase) {
+      allCharacters += uppercaseCharacters
+    }
+
+    // For loop, Math.Floor rounds down to nearest whole number, math random times var allCharacters length creates random string including all properties that the user selects
+    for (let i = 0; i < passwordLength; i++) {
+      finalPassword += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length));
+    }
+    return finalPassword
+  }
+}
+
+// Pseudocode:
 
 // var types = window.confirm("Click OK to confirm inlcuding special characters")
-
 // var numbers = window.confirm("Click OK to confirm inlcuding numeric characters")
-
 // var lowercase = window.confirm("Click OK to confirm inlcuding lowercase characters")
-
 // var uppercase = window.confirm("Click OK to confirm inlcuding uppercase characters")
 
 // var specialCharacters = "!@#$%^&*-?"
@@ -30,10 +70,7 @@ var generatePassword = function () {
 // If lowercase is true include lowercaseCharacters
 // If uppercase is true include uppercaseCharacters
 
-
 // for (let i = ; i < ; i++) {
-
-generatePassword()
 
 // Write password to the #password input
 function writePassword() {
@@ -45,18 +82,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
